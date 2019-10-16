316b6f709430d83d1a9cc2ad41f9e038151fd19d
Stelmaszv <stelmaszv@gmail.com>
2019-10-16 12:33:38 +0200

    Created ORM

---
+++ src/Entity/News.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NewsRepository")
+ */
+class News
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $tittle;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $toshow;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTittle(): ?string
+    {
+        return $this->tittle;
+    }
+
+    public function setTittle(string $tittle): self
+    {
+        $this->tittle = $tittle;
+
+        return $this;
+    }
+
+    public function getToshow(): ?int
+    {
+        return $this->toshow;
+    }
+
+    public function setToshow(int $toshow): self
+    {
+        $this->toshow = $toshow;
+
+        return $this;
+    }
+}
